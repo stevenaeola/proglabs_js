fd92c9ccad2375837efa816a60f9c5bc88cd56ba
sglre6355 <sglre6355@gmail.com>
2023-11-17 14:30:12 +0000

    Add basic web server function

--- node_intro/hello.js
+++ node_intro/hello.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+
+const http = require("http");
+
+const hostname = "100.112.100.112";
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/plain");
+    res.end("Hello World!");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
